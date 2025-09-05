027c94252db6718abef210668e402049ba4d388b
hgscribner07 <hgscribner07@gmail.com>
2025-09-05 12:18:22 -0500

    using namespace

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-
+using namespace std;
 int main() {
 
     auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    cout << "Hello and welcome to " << lang << "!\n";
 
     for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
+        cout << "i = " << i << endl;
     }
 
     return 0;
