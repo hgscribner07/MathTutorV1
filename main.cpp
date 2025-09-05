d5f623743ba93104e734bf88633a3f003ace84c5
hgscribner07 <hgscribner07@gmail.com>
2025-09-05 12:35:01 -0500

    I added the introduction!

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/***********************************************************************************************************************
+* Title......: Math Tutor V1
+* Programmers: Hannah Scribner, Muhammad Bilal, Isiaha Black-Marhslek
+* Date.......: 2025.09.5
+* Github Repo: https://github.com/hgscribner07/MathTutorV1
+* Description: An easy math tutor. designed to help young children with their math abilites. It displays the programs
+*  intro, asks for the users name, shows simple questions, and then displays an end-of-program message.
+***********************************************************************************************************************/
+
+
+
 #include <iostream>
 using namespace std;
 int main() {
